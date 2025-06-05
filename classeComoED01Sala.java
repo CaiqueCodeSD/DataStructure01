e9bcf1a0083331d4b09aa74916269da9b006b3c5
Caíque <149615073+CaiqueCodeSD@users.noreply.github.com>
2025-06-05 20:21:25 -0300

    Update classeComoED01Sala.java

--- classeComoED01Sala.java
+++ classeComoED01Sala.java
@@ -2,6 +2,24 @@
 
 public class classeComoED01Sala {
 
+   public static void main (String[] args) {
 
+      public static void buscarNome(String nome) {
+
+         boolean encontrou = false;
+
+         for (int i = 0; i < V.length; i++) {
+            if (V[i].nome.toLowerCase(); == nome.toLowerCase()) {
+               sout("Nome encontrado ");
+               encontrou = true;
+            }
+         }
+
+         if (!encontrou) {
+            sout("Nome não encontrado)";
+         }
+      }
+
+   }
 
 }
