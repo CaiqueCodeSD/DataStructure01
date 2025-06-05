27f6d060f1f5ad6f28c6cb1c2b6dd0f9d91156c5
Caíque <149615073+CaiqueCodeSD@users.noreply.github.com>
2025-06-05 20:25:24 -0300

    Update classeComoED01Sala.java

--- classeComoED01Sala.java
+++ classeComoED01Sala.java
@@ -1,8 +1,25 @@
 /* Busca por nome / Busca por codigo / ordena por nome / ordena por codigo */
 
+class Disciplina {
+
+   String nome;
+   int cod;
+
+   Disciplina (String nome, int cod) {
+      this.nome = nome;
+      this.cod = cod
+   }
+
+}
+
 public class classeComoED01Sala {
 
    public static void main (String[] args) {
+
+   String nome;
+   int cod;
+
+   Disciplina[] V = new Disciplina[5];
 
       public static void buscarNome(String nome) {
 
