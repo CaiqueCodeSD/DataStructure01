099588a45c53281fd0e1531e948cfa7639b872ea
Caíque <149615073+CaiqueCodeSD@users.noreply.github.com>
2025-06-06 12:09:03 -0300

    Update classeComoED01Sala.java

--- classeComoED01Sala.java
+++ classeComoED01Sala.java
@@ -9,58 +9,109 @@ class Disciplina {
 
    Disciplina (String nome, int cod) {
       this.nome = nome;
-      this.cod = cod
+      this.cod = cod;
    }
 
 }
 
-public class classeComoED01Sala {
+public class ClasseComoED01Sala {
 
-   public static void main (String[] args) {
-
-   Scanner sc = new Scanner(System.in);
-
-   String nome;
-   int cod;
-
-   Disciplina[] V = new Disciplina[5];
-
-   for (int i = 0; i < V.length; i++) {
-      sout("Nome: ");
-      nome = sc.nextLine();
-      sout("Cod: ");
-      cod = sc.nextInt();
-      sc.nextLine();
-      V[i] = new Disciplina(nome, cod);
-   }
-
+   //método para buscar nome
    public static void buscarNome(String nome, Disciplina V[]) {
 
       boolean encontrou = false;
 
       for (int i = 0; i < V.length; i++) {
-         if (V[i].nome.toLowerCase(); == nome.toLowerCase()) {
-            sout("Nome encontrado ");
+         if (V[i].nome.equalsIgnoreCase(nome)) {
+            System.out.println("\n" + "Nome encontrado!" + "\n");
             encontrou = true;
          }
       }
 
       if (!encontrou) {
-         sout("Nome não encontrado)";
+         System.out.println("\n" + "Não encontrado" + "\n");
       }
    }
 
-   public static void buscarCod (Disciplina V[], int c){
-
+   //método para buscar código
+   public static void buscarCod (Disciplina V[], int c) {
       for (int i = 0; i < V.length; i++) {
-         if (V[i] == c) {
-            sout("Achou");
+         if (V[i].cod == c) {
+            System.out.println("\n" + "Achou!" + "\n");
          }
       }
+   }
+
+   //método para ordenar por nome
+   public static void ordenarNomes (Disciplina V[], String nome) {
+
+      System.out.println("--- Nomes ordenados: ---");
+
+      for (int i = 0; i < V.length - 1; i++) {
+         for (int j = i + 1; j < V.length; j++) {
+            if (V[i].nome.compareToIgnoreCase(V[j].nome) > 0) {
+               Disciplina temp = V[i];
+               V[i] = V[j];
+               V[j] = temp;
+            }
+         }
+        
+      }
+      for (int i = 0; i < V.length; i++) {
+         System.out.println(V[i].nome);
+      }
+   }
+
+   //método para ordenar por codigo
+   public static void ordenarCodigos (Disciplina V[], int cod) {
+
+      System.out.println("--- Códigos ordenados: ---");
+
+      for (int i = 0; i < V.length - 1; i++) {
+         for (int j = i + 1; j < V.length; j++) {
+            if (V[i].cod > V[j].cod) {
+               Disciplina temp = V[i];
+               V[i] = V[j];
+               V[j] = temp;
+            }
+         }
+      }
+      
+      for (int i = 0; i < V.length; i++) {
+         System.out.println(V[i].cod);
+      }
 
    }
 
 
+   public static void main (String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String nome;
+        int cod;
+
+        Disciplina[] V = new Disciplina[5];
+
+        for (int i = 0; i < V.length; i++) {
+            System.out.print("Insira o nome: ");
+            nome = sc.nextLine();
+            System.out.print("Informe o código: ");
+            cod = sc.nextInt();
+            sc.nextLine();
+            V[i] = new Disciplina(nome, cod);
+        }
+
+        System.out.println("--- Qual nome deseja buscar? ---");
+        nome = sc.nextLine();
+        buscarNome(nome, V);
+
+        System.out.println("--- Qual código está buscando? ---");
+        cod = sc.nextInt();
+        buscarCod(V, cod);
+
+        ordenarNomes(V, nome);
+        ordenarCodigos(V, cod);
 
    }
 
